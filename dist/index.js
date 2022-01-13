b0f621017049fd37054f1f0609ae9cce4ac18b2d
Brian Staheli <bstah99@hotmail.com>
2022-01-12 17:30:15 -0700

    backend done

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,78 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const node_cache_1 = __importDefault(require("node-cache"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 8080; // default port to listen
-// define a route handler for the default home page
+const cache = new node_cache_1.default();
+const port = process.env.PORT || 8080; // default port to listen
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.raw());
 app.get("/", (req, res) => {
     res.send("Hello world!");
+});
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let users = yield axios_1.default.get("https://reqres.in/api/users?page=2");
+        let usersData = users.data.data;
+        if (users) {
+            let success = true;
+            usersData.forEach((item) => {
+                const isSuccess = cache.set(item.id, item);
+                if (!isSuccess)
+                    success = false;
+            });
+            if (success)
+                return res.json({ message: "successfully got users", data: usersData });
+            return res.status(500).json({ message: "unable to set cache", data: usersData });
+        }
+        return res.status(500).json({ message: "unable to get users" });
+    }
+    catch (error) {
+        return error;
+    }
+}));
+app.post("/users/create", (req, res) => {
+    let newUser = Object.assign({}, req.body);
+    if (newUser.id) {
+        const success = cache.set(newUser.id, newUser);
+        if (success)
+            return res.json({ message: "New user created", data: newUser });
+        return res.status(500).json({ message: "unable to create user" });
+    }
+    return res.status(400).json({ message: "unable to create user" });
+});
+app.put("/users/update", (req, res) => {
+    let updateUser = Object.assign({}, req.body);
+    if (updateUser.id) {
+        let oldUser = cache.take(updateUser.id);
+        const success = cache.set(updateUser.id, updateUser);
+        if (success)
+            return res.json({ message: "successfully updated user", data: updateUser });
+        cache.set(oldUser.id, oldUser);
+        return res.status(500).json({ message: "unable to update user" });
+    }
+    return res.status(400).json({ message: "unable to update user" });
+});
+app.delete("/users/delete/:id", (req, res) => {
+    let value = cache.del(req.params.id);
+    if (value > 0)
+        return res.json({ message: "success", data: value });
+    return res.status(400).json({ message: "unable to delete user" });
 });
 // start the Express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import axios from "axios";
 import NodeCache from "node-cache";
+import bodyParser from "body-parser";
 
 const app = express();
 const cache = new NodeCache();
 const port = process.env.PORT || 8080; // default port to listen
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 interface User {
     id: number;
@@ -19,51 +24,57 @@ app.get( "/", ( req, res ) => {
 } );
 
 app.get("/users", async (req, res) =>{
-    let users = await axios.get("reqres.in/api/users?page=2");
-    if(users.data){
+    try{
+    let users = await axios.get("https://reqres.in/api/users?page=2");
+    let usersData:Array<User> = users.data.data
+    if(users){
         let success = true;
-        users.data.array.forEach((item:User) => {
+        usersData.forEach((item:User) => {
             const isSuccess = cache.set(item.id, item)
             if(!isSuccess)
                 success = false;
         });
         if(success)
-            return res.json({message: "successfully got users",data:users.data});
-        return res.json({message: "unable to set cache", data: users.data});
+            return res.json({message: "successfully got users",data:usersData});
+        return res.status(500).json({message: "unable to set cache", data: usersData});
     }
-    return res.json({message: "unable to get users"});
+    return res.status(500).json({message: "unable to get users"});
+    } catch(error){
+        return error;
+    }
 })
 
-app.post("users/create", (req, res) => {
+app.post("/users/create", (req, res) => {
     let newUser:User = {...req.body}
-    if(newUser){
+    if(newUser.id){
         const success = cache.set(newUser.id, newUser);
         if(success)
             return res.json({message:"New user created", data: newUser})
-        return res.json({message: "unable to create user"})
+        return res.status(500).json({message: "unable to create user"})
     }
-    return res.json({message: "unable to create user"})
+    return res.status(400).json({message: "unable to create user"})
 })
 
-app.put("users/update", (req, res) => {
+app.put("/users/update", (req, res) => {
     let updateUser:User = {...req.body};
-    if(updateUser){
+    
+    if(updateUser.id){
         let oldUser:User = cache.take(updateUser.id);
         const success = cache.set(updateUser.id, updateUser);
         if(success)
             return res.json({message: "successfully updated user", data: updateUser});
         
         cache.set(oldUser.id, oldUser);
-        return res.json({message: "unable to update user"});
+        return res.status(500).json({message: "unable to update user"});
     }
-    return res.json({message: "unable to update user"});
+    return res.status(400).json({message: "unable to update user"});
 })
 
-app.delete("/users/:id", (req, res) => {
+app.delete("/users/delete/:id", (req, res) => {
     let value = cache.del(req.params.id)
     if(value > 0)
         return res.json({message: "success", data: value})
-    return res.json({message: "unable to delete user"});
+    return res.status(400).json({message: "unable to delete user"});
 })
 
 // start the Express server
